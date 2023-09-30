00a380f337ef0fcdadd72adaa2fe7b95f593b221
david steinsland <david@davidsteinsland.net>
2023-09-30 17:26:47 +0200

    bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 val javaVersion = "17"
-val kotlinVersion = "1.8.10"
-val ktorVersion = "2.2.4"
-val logbackClassicVersion = "1.4.6"
-val logbackEncoderVersion = "7.3"
+val ktorVersion = "2.3.4"
+val logbackClassicVersion = "1.4.11"
+val logbackEncoderVersion = "7.4"
 
-val junitJupiterVersion = "5.9.1"
+val junitJupiterVersion = "5.10.0"
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.9.10"
 }
 
 repositories {
@@ -16,11 +15,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2022092314391663936769.9d5d33074875")
+    implementation("com.github.navikt:rapids-and-rivers:2023093008351696055717.ffdec6aede3d")
 
     implementation("org.flywaydb:flyway-core:9.7.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.postgresql:postgresql:42.5.0")
+    implementation("org.postgresql:postgresql:42.6.0")
     implementation("com.github.seratch:kotliquery:1.9.0")
 
     testImplementation("org.testcontainers:postgresql:1.17.5")
@@ -50,9 +49,8 @@ tasks {
 
         doLast {
             configurations.runtimeClasspath.get().forEach {
-                val file = File("$buildDir/libs/${it.name}")
-                if (!file.exists())
-                    it.copyTo(file)
+                val file = File("${layout.buildDirectory.get()}/libs/${it.name}")
+                if (!file.exists()) it.copyTo(file)
             }
         }
     }
