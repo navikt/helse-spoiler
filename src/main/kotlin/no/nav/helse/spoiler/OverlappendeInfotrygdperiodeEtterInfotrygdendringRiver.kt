569b67f197dba8db1fbb62c3de0b7f77b534d89f
Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
2024-03-11 12:45:27 +0100

    Publiserer til Slack om det legges inn overlappende med avsluttet periode
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- src/main/kotlin/no/nav/helse/spoiler/OverlappendeInfotrygdperiodeEtterInfotrygdendringRiver.kt
+++ src/main/kotlin/no/nav/helse/spoiler/OverlappendeInfotrygdperiodeEtterInfotrygdendringRiver.kt
@@ -7,6 +7,7 @@ import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.helse.rapids_rivers.asLocalDateTime
+import java.time.LocalDate
 import java.util.UUID
 
 class OverlappendeInfotrygdperiodeEtterInfotrygdendringRiver(
@@ -41,5 +42,37 @@ class OverlappendeInfotrygdperiodeEtterInfotrygdendringRiver(
         overlappendeInfotrygdperiodeEtterInfotrygdendringDao.lagre(overlappendeInfotrygdperiodeEtterInfotrygdendring)
 
         log.info("Lagret data fra overlappende_infotrygdperiode_etter_infotrygdendring i databasen")
+
+        val vedtaksperiodetilstand = packet["vedtaksperiodetilstand"].asText()
+        if (!erPeriodeTidligereAvsluttet(vedtaksperiodetilstand)) return log.info("Lager ikke alarm etter overlappende Infotrygdperiode i tilstand $vedtaksperiodetilstand")
+        val vedtaksperiodeId = packet["vedtaksperiodeId"].let { UUID.fromString(it.asText()) }
+        val periode = packet["vedtaksperiodeFom"].asLocalDate() to packet["vedtaksperiodeTom"].asLocalDate()
+        val slackmelding = slackmelding(vedtaksperiodeId, vedtaksperiodetilstand, periode)
+        context.publish(lagSlackmelding(slackmelding).toJson())
+    }
+
+    private fun erPeriodeTidligereAvsluttet(vedtaksperiodetilstand: String) : Boolean {
+        return when(vedtaksperiodetilstand) {
+            "AVSLUTTET",
+            "AVVENTER_REVURDERING",
+            "AVVENTER_HISTORIKK_REVURDERING",
+            "AVVENTER_VILKÅRSPRØVING_REVURDERING",
+            "AVVENTER_SIMULERING_REVURDERING",
+            "AVVENTER_GODKJENNING_REVURDERING" -> true
+            else -> false
+        }
+    }
+
+    private fun slackmelding(
+        vedtaksperiodeId: UUID,
+        vedtaksperiodetilstand: String,
+        periode: Pair<LocalDate, LocalDate>
+    ) : String {
+        return "Huff! Det er lagt inn en overlappende periode i Infotrygd :sadge: Hva skal vi gjøre med dette? \n\n" +
+                "Det gjelder vedtaksperiodeId $vedtaksperiodeId i tilstand $vedtaksperiodetilstand for periode ${periode.first} - ${periode.second}"
+    }
+
+    private fun lagSlackmelding(melding: String) : JsonMessage {
+        return JsonMessage.newMessage("slackmelding", mapOf("melding" to melding))
     }
 }
