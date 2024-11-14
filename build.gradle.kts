6e44d201898d2b0768ae9e0e1185e8894d8f82be
david steinsland <david@davidsteinsland.net>
2024-11-14 09:36:50 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val flywayVersion = "10.4.1"
 val hikariCPVersion = "5.1.0"
 val kotliQueryVersion = "1.9.0"
 val rapidsAndRiversVersion = "2024111408081731568123.6619b48b7043"
-val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
+val tbdLibsVersion = "2024.11.14-07.56-63a8564f"
 
 plugins {
     kotlin("jvm") version "2.0.21"
