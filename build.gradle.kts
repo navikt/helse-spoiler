4dcc881ffb60e8083fdaa3825e80c56b8a996ea0
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-17 13:01:50 +0200

    Bytter baseimage og bumper dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val hikariCPVersion = "6.3.0"
 val kotliQueryVersion = "1.9.0"
 val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
-val postgresqlVersion = "42.7.5"
+val postgresqlVersion = "42.7.7"
 
 plugins {
     kotlin("jvm") version "2.2.10"
